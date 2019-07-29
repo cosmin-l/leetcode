ccc5654bdc2647ecc99828b3ede7837bd3e0ddb6
lekki <lechintan.cosmin@gmail.com>
2019-07-29 16:54:54 +0300

    small refactor

--- src/org/cl/problem26/Solution.java
+++ src/org/cl/problem26/Solution.java
@@ -5,8 +5,7 @@ public class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
         int current = nums[0];
-        int index = 1;
-        int cursor = 1;
+        int index, cursor = 1;
         while (index < nums.length) {
             if (nums[index] != current) {
                 nums[cursor] = nums[index];
